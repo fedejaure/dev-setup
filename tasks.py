58652740f9347b8ebae72b3a29ed31bb03d7cf12
Federico Jaureguialzo <fedejaure@gmail.com>
2022-09-11 18:16:39 +0200

    Remove flakehell in favor o flakeheaven

--- tasks.py
+++ tasks.py
@@ -1,5 +1,4 @@
-"""
-Tasks for maintaining the project.
+"""Tasks for maintaining the project.
 
 Execute 'invoke --list' for guidance on using Invoke
 """
@@ -87,7 +86,7 @@ def format_(c, check=False):
 def flake8(c):
     # type: (Context) -> None
     """Run flake8."""
-    _run(c, f"poetry run flakehell lint {PYTHON_TARGETS_STR}")
+    _run(c, f"poetry run flakeheaven lint {PYTHON_TARGETS_STR}")
 
 
 @task()
